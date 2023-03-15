71de9e0a3a32f04f617316a92c20db65745ecbde
20201TIIMI0225 <elisaaj.es@gmail.com>
2023-03-15 09:08:56 -0300

    elisaaj.es@gmail.com

--- app/src/main/java/jesus/de/andrade/elisa/primeiraapp/MainActivity.java
+++ app/src/main/java/jesus/de/andrade/elisa/primeiraapp/MainActivity.java
@@ -19,19 +19,27 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+//        APLICAÇÃO  EXERCÍCIO:
+
+//        Pegue o texto desejado de uma tela e faça-o aparecer em outra.
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        CRIANDO VARIÁVEL BOTÃO
         Button btnEnviar = findViewById(R.id.btnEnviar);
+//        Fzendo o sistema esperar o click no botão
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
+//            QUANDO O BOTÃO FOR PRESSIONADO
             public void onClick(View view) {
                 // What can I place here?
-                EditText etTexto = findViewById(R.id.etDigiteAqui);
-                String tText = etTexto.getText().toString();
-                Intent i = new Intent(MainActivity.this, NextActivity.class);
-                i.putExtra("text", tText);
-                startActivity(i);
+                EditText etTexto = findViewById(R.id.etDigiteAqui); // cria a  VARIAVEL que ARMAZENA o elemento editText que possui o texto
+                String tText = etTexto.getText().toString(); // pega o TEXTO da variavel anterior e armazena em uma string
+                Intent i = new Intent(MainActivity.this, NextActivity.class); // cria uma variavel intent que fará a comunicação entre ambas as telas
+                i.putExtra("text", tText); // adiciona o conteúdo que necessita ser enviado
+                startActivity(i); // inicializa o intent
 
             }
         });
--- app/src/main/java/jesus/de/andrade/elisa/primeiraapp/NextActivity.java
+++ app/src/main/java/jesus/de/andrade/elisa/primeiraapp/NextActivity.java
@@ -12,10 +12,12 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
-        Intent i = this.getIntent();
-        String finaltext = i.getStringExtra("text");
-        TextView tvText = findViewById(R.id.tvTexto);
-        tvText.setText(finaltext);
+
+
+        Intent i = this.getIntent(); // cria uma variavel intent para receber o intent da outra tela
+        String finaltext = i.getStringExtra("text"); //pega o conteúo desejado do intent
+        TextView tvText = findViewById(R.id.tvTexto); // encontra e armazena o elemento que será modificado
+        tvText.setText(finaltext); //muda o texto da variável tcText
 
     }
 }
